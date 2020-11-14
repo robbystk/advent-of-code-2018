39939f5f8582af69a3a115186c0ef7a59a208905
Robby Stokoe <robbystk@gmail.com>
2020-11-13 17:39:12 -0700

    Determine time sequence with multiple workers

--- 07sleigh/sleigh.py
+++ 07sleigh/sleigh.py
@@ -68,15 +68,92 @@ def determine_step_sequence(steps, dependencies):
 
     return step_sequence
 
+def seconds_for(step):
+    offset = 0
+    return ord(step) - 64 + offset
+
+class Worker:
+    def __init__(self):
+        self.step = None
+        self.seconds_left = 0
+
+    def work_on(self, step):
+        if self.step is not None:
+            raise "Worker is already occupied"
+        self.step = step
+        self.seconds_left = seconds_for(step)
+
+    def tick(self, coordinator):
+        if self.step is not None:
+            self.seconds_left -= 1
+            if self.seconds_left <= 0:
+                coordinator.finish_step(self.step)
+                self.step = None
+
+    def is_available(self):
+        return self.step is None
+
+    def __repr__(self):
+        return f"Working on {self.step} for {self.seconds_left} more seconds"
+
+class Coordinator:
+    def __init__(self, workers, steps, dependencies):
+        self.workers = workers
+        self.steps = steps.copy()
+        self.to_do = steps.copy()
+        self.dependencies = dependencies
+        self.seconds = 0
+        self.done = set()
+
+        self.assign_work()
+
+    def finish_step(self, step):
+        print(f"Finished step {step}")
+        self.done.add(step)
+
+    def assign_available_worker(self, step):
+        for worker in self.workers:
+            if worker.is_available():
+                worker.work_on(step)
+                self.to_do.remove(step)
+                break
+
+    def assign_work(self):
+        for step in sorted(self.to_do):
+            deps = self.dependencies.get_deps(step)
+            if is_empty(deps - self.done):
+                self.assign_available_worker(step)
+
+
+    def tick(self):
+        # advance workers
+        for worker in self.workers:
+            worker.tick(self)
+
+        self.assign_work()
+        self.seconds += 1
+        print(self.seconds, self.workers, self.to_do, self.done)
+
+    def run(self):
+        while(not self.done >= self.steps):
+            self.tick()
+
 def main():
     steps, dependencies = get_steps_and_dependencies()
 
     print(dependencies)
     print(sorted(steps))
 
-    step_sequence = determine_step_sequence(steps, dependencies)
+    step_sequence = determine_step_sequence(steps.copy(), dependencies)
 
     print(''.join(step_sequence))
+
+    N_workers = 2
+    workers = [Worker() for _ in range(N_workers)]
+
+    coordinator = Coordinator(workers, steps, dependencies)
+    coordinator.run()
+    print(coordinator.seconds)
 
 if __name__ == "__main__":
     main()
