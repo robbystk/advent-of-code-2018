7f25cfdd112b16453357848356d19ae6310f1839
Robby Stokoe <robbystk@gmail.com>
2020-11-15 08:15:12 -0700

    Complete day 8 part 2

--- 08tree/tree.py
+++ 08tree/tree.py
@@ -7,23 +7,44 @@ def input():
 
 class Node:
     def __init__(self, input):
-        children_count = int(next(input))
-        metadata_count = int(next(input))
+        self.children_count = int(next(input))
+        self.metadata_count = int(next(input))
 
-        self.children = [Node(input) for _ in range(children_count)]
-        self.metadata = [int(next(input)) for _ in range(metadata_count)]
+        self.children = [Node(input) for _ in range(self.children_count)]
+        self.metadata = [int(next(input)) for _ in range(self.metadata_count)]
 
     def __repr__(self):
-        return f"Children:\n\t{self.children}\nMetadata:\n\t{self.metadata}\n"
+        children_str = ''.join([repr(child) for child in self.children])
+        return f"<node metadata=\"{self.metadata}\">{children_str}</node>"
 
     def metadata_sum(self):
         return sum([child.metadata_sum() for child in self.children]) + sum(self.metadata)
+
+    def value(self):
+        if self.children_count == 0:
+            value = sum(self.metadata)
+            return value
+        else:
+            rv = 0
+            for idx in self.metadata:
+                if idx > 0 and idx <= self.children_count:
+                    rv += self.children[idx - 1].value()
+            return rv
 
 def main():
     inp = input()
     root = Node(inp)
 
-    print(root.metadata_sum())
+    try:
+        print(next(inp))
+    except StopIteration as e:
+        pass
+    else:
+        raise "didn't reach end of input"
+
+    print(f"metadata sum: {root.metadata_sum()}")
+
+    print(f"value: {root.value()}")
 
 if __name__ == '__main__':
     main()
