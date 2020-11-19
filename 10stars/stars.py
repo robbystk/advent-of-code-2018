be1d2814cf53557d8b6a27c2ba477a957baf7eea
Robby Stokoe <robbystk@gmail.com>
2020-11-18 21:45:06 -0700

    Add a way to print the field of points

--- 10stars/stars.py
+++ 10stars/stars.py
@@ -19,6 +19,20 @@ class Stars:
     def bounding_box_size(self):
         return self.position_list.max(axis=0) - self.position_list.min(axis=0)
 
+    def __str__(self):
+        start = self.position_list.min(axis=0) - np.array([1, 1])
+        end = self.position_list.max(axis=0) + np.array([1, 1])
+        dimensions = self.bounding_box_size() + np.array([2,2])
+
+        picture = [[' ' for _ in range(dimensions[0])] for _ in range(dimensions[1])]
+
+        for position in self.position_list:
+            offset_position = position - start
+            picture[offset_position[1]][offset_position[0]] = '#'
+
+        return '\n'.join([''.join(row) for row in picture])
+
+
 def main():
     line_pattern = re.compile('\Aposition=< *(-?\d+),  *(-?\d+)> velocity=< *(-?\d+),  *(-?\d+)>')
 
@@ -34,6 +48,8 @@ def main():
     starfield = Stars(position_list, velocity_list)
     
     print(starfield.bounding_box_size())
+
+    print(starfield)
 
 if __name__ == '__main__':
     main()
